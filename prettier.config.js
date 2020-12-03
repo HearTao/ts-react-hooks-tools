4ab13534e167294c2b2767d85bc5d9ac1345bb72
kingwl <kingwenlu@gmail.com>
2020-12-04 00:34:49 +0800

    compact prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,22 +1,13 @@
 module.exports = {
     parser: 'typescript',
-
     semi: true,
-
     singleQuote: true,
-
     jsxSingleQuote: false,
-
     bracketSpacing: true,
-
     tabWidth: 4,
-
     useTabs: false,
-
     trailingComma: 'none',
-
     proseWrap: 'always',
-
     arrowParens: 'avoid',
     endOfLine: 'crlf'
 };
